c39d3bc0cb9b573baeb92d44f6fdb6d63b685c8f
Gerry Chen <gerry.chen2015@gmail.com>
2025-07-13 13:16:58 +0200

    Update cloudflare proxy CORS

--- amadeus-proxy/src/index.js
+++ amadeus-proxy/src/index.js
@@ -4,9 +4,17 @@
  * - Run `npm run dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
  * - Run `npm run deploy` to publish your worker
+ * 
+ * - Use `cd amadeus-proxy; wrangler deploy` to deploy to Cloudflare.
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+
+const ALLOWED_ORIGINS = [
+  'https://gchenfc.github.io',
+  'https://gerry-chen.com',
+  // 'null', // Uncomment this for local testing with file:// URLs
+];
 
 export default {
   async fetch(request, env, ctx) {
@@ -14,6 +22,18 @@ export default {
 
     if (url.pathname !== "/token") {
       return new Response("Not Found", { status: 404 });
+    }
+
+    const origin = request.headers.get("Origin");
+    const allowOrigin = ALLOWED_ORIGINS.includes(origin) ? origin : "";
+    if (!allowOrigin) {
+      return new Response("Blocked by CORS", {
+        status: 403,
+        headers: {
+          "Access-Control-Allow-Origin": allowOrigin,
+          "Vary": "Origin"
+        }
+      });
     }
 
     const res = await fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
@@ -33,7 +53,8 @@ export default {
     return new Response(JSON.stringify(data), {
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": allowOrigin,
+        "Vary": "Origin"
       }
     });
   }
